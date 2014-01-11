6d0008349ba7a388d2a3f27dc132aec5ffc4a476
weerachai <wa@engr.tu.ac.th>
2014-01-11 12:01:21 +0700

    modify Movie model for test 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,13 @@
 class Movie < ActiveRecord::Base
  
+  class Movie::InvalidKeyError < StandardError ; end
+ 
   def self.find_in_tmdb(string)
-    TmdbMovie.find(:title => string)
+    begin
+      TmdbMovie.find(:title => string)
+    rescue ArgumentError => tmdb_error
+      raise Movie::InvalidKeyError, tmdb_error.message
+    end      
   end
  
   # rest of file elided for brevity
