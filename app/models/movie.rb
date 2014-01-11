bb76945ce29651f1179f6406b35e0f15bfcaf734
weerachai <wa@engr.tu.ac.th>
2014-01-11 13:02:24 +0700

    add api key

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,13 +2,17 @@ class Movie < ActiveRecord::Base
  
   class Movie::InvalidKeyError < StandardError ; end
  
+  def self.api_key
+    'cc4b67c52acb514bdf4931f7cedfd12b' # replace with YOUR Tmdb key
+  end
+ 
   def self.find_in_tmdb(string)
+    Tmdb.api_key = self.api_key
     begin
       TmdbMovie.find(:title => string)
     rescue ArgumentError => tmdb_error
       raise Movie::InvalidKeyError, tmdb_error.message
-    end      
+    end
   end
- 
   # rest of file elided for brevity
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,6 +8,7 @@ describe Movie do
       Movie.find_in_tmdb('Inception')
     end
     it 'should raise an InvalidKeyError with no API key' do
+      Movie.stub(:api_key).and_return('')
       lambda { Movie.find_in_tmdb('Inception') }.
         should raise_error(Movie::InvalidKeyError)
     end
